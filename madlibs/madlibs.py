6ad9a277f10675ab9065832418fee85f0d58f2f7
Aarya Sarfare <aarya.work005@gmail.com>
2025-11-11 22:28:37 +0530

    Initial commit

---
+++ madlibs/madlibs.py
@@ -0,0 +1,9 @@
+adjective1 = input("Provide an adjective: ").lower()
+adjective2 = input("Provide another adjective: ").lower()
+verb1 = input("Provide a verb: ").lower()
+verb2 = input("Provide another verb: ").lower()
+
+paragraph = f"A {adjective1} option for complete beginners without any experience to {verb1}. \
+If you're looking for a {adjective2} intro to this very deep language, I have to {verb2} this book."
+
+print(paragraph)
