e6adf2e105fa4d873867d1cf75636bc5856089c0
Aarya Sarfare <aarya.work005@gmail.com>
2025-11-20 10:55:26 +0530

    made a dice stimulator

---
+++ simple_projects/dice_stimulator.py
@@ -0,0 +1,71 @@
+import random
+
+dice_drawing = {
+        1: (
+            "-------",
+            "|     |",
+            "|  o  |",
+            "|     |",
+            "-------",
+        ),
+        2: (
+            "-------",
+            "|o    |",
+            "|     |",
+            "|    o|",
+            "-------",
+        ),
+        3: (
+            "-------",
+            "|o    |",
+            "|  o  |",
+            "|    o|",
+            "-------",
+        ),
+        4: (
+            "-------",
+            "|o   o|",
+            "|     |",
+            "|o   o|",
+            "-------",
+        ),
+        5: (
+            "-------",
+            "|o   o|",
+            "|  o  |",
+            "|o   o|",
+            "-------",
+        ),
+        6: (
+            "-------",
+            "|o   o|",
+            "|o   o|",
+            "|o   o|",
+            "-------",
+        ),
+
+    }
+
+list_of_keys = []
+for key in dice_drawing.keys():
+    list_of_keys.append(key)
+
+def roll_dice():
+    dice1 = random.choice(list_of_keys)
+    dice2 = random.choice(list_of_keys)
+    for i in dice_drawing[dice1]:
+        print(i)
+    for i in dice_drawing[dice2]:
+        print(i)
+
+while True:
+    print("Welcome To Dice Stimulator!\n")
+    user_input = input("Want to roll the dice? Type Yes or No: ").lower()
+    if user_input == 'yes':
+        roll_dice()
+    elif user_input == 'no':
+        print("Thank you for using the Dice Stimulator!")
+        exit()
+    else:
+        print("Please provide a valid input.")
+        exit()
