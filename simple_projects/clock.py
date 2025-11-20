eac39fb851a92304281bb97de77a7e474cec022e
Aarya Sarfare <aarya.work005@gmail.com>
2025-11-20 13:37:52 +0530

    correcting bug in clock.py

--- simple_projects/clock.py
+++ simple_projects/clock.py
@@ -1,6 +1,6 @@
 import time
 
-current_time = time.gmtime()
+current_time = time.localtime()
 
 current_date = current_time.tm_mday
 current_month = current_time.tm_mon
@@ -8,10 +8,7 @@ current_year = current_time.tm_year
 
 print(f"Today's date in DD/MM/YYYY format is {current_date}/{current_month}/{current_year}.")
 
-for i in range(10):
-    current_time = time.gmtime()
-    current_time_minutes = current_time.tm_min
-    current_time_hours = current_time.tm_hour
-    current_time_seconds = current_time.tm_sec
-    print(f"Time: {current_time_hours}:{current_time_minutes}:{current_time_seconds}")
-    time.sleep(1)
+current_time_minutes = current_time.tm_min
+current_time_hours = current_time.tm_hour
+current_time_seconds = current_time.tm_sec
+print(f"Time: {current_time_hours}:{current_time_minutes}:{current_time_seconds}")
