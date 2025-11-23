5fef096d43618f6fd36aa3d3ef3bb635f9915516
Aarya Sarfare <aarya.work005@gmail.com>
2025-11-23 10:11:48 +0530

    Refactoring my password_generator code

--- password_generator/password_generate.py
+++ password_generator/password_generate.py
@@ -1,6 +1,6 @@
 import random
 import string
-
+import validate_user_input as validate_user_input
 
 user_choice = []
 lowercause_alphabets = string.ascii_lowercase
@@ -8,45 +8,20 @@ uppercase_alphabets = string.ascii_uppercase
 numbers = string.digits
 symbols = string.punctuation
 
-def validate_length_of_characters(number_of_characters):
-    if number_of_characters < 8 or number_of_characters > 32:
-        print("Please provide a number between 8 and 32.")
-        exit()
-    else:
-        pass
-
 def take_user_choice():
 
-    number_of_characters = 8
-    include_numbers = -1
-    include_lower_case = -1
-    include_upper_case = -1
-    include_symbols = -1
+    print("Enter '1' to say yes, and '0' to say no.\n")
 
-    print("Please input password length atleast 8 characters and atmost 32 characters.")
-    try:    
-        number_of_characters = int(input("Length of Password: "))
-        validate_length_of_characters(number_of_characters)
-    except ValueError:
-        print("Invalid input. Please provide a number between 8 and 32.")
-        exit()
-
-    print("Enter '1' to say yes, and '0' to say no.")
-
-    try:
-        include_numbers = int(input("Do you want to include numbers: "))
-        include_lower_case = int(input("Do you want to include lower case characters: "))
-        include_upper_case = int(input("Do you want to include upper case characters: "))
-        include_symbols = int(input("Do you want to include symbols: "))
-    except ValueError:
-        print("Invalid Input. Please input either 1 or 0.")
-        exit()
+    number_of_characters = validate_user_input.take_and_validate_length_of_characters()
+    include_numbers = validate_user_input.take_and_validate_input_numbers()
+    include_lower_case = validate_user_input.take_and_validate_input_lower_case()
+    include_upper_case = validate_user_input.take_and_validate_input_upper_case()
+    include_symbols = validate_user_input.take_and_validate_input_symbols()
 
     if include_numbers == 0 and include_lower_case == 0 \
         and include_upper_case == 0 and include_symbols == 0:
         print("You need to at least select one option.")
         print("Please re-run the program.")
-        exit()
 
     if include_numbers == 1:
         user_choice.append(string.digits)
@@ -65,7 +40,7 @@ def take_user_choice():
     return user_choice_str, number_of_characters
 
 def generate_password(user_choice_str, number_of_characters):
-
+    """Generates a randomized string from the set of choices user has made."""
     password = []
     for i in range(number_of_characters):
         password.append(random.choice(user_choice_str))
@@ -74,12 +49,16 @@ def generate_password(user_choice_str, number_of_characters):
 
 def run():
     """Connects all the functions and runs them all together to effectively generate the password."""
-    print("Welcome to Password Generator!\n")
-    user_choice, number_of_characters = take_user_choice()
-    password = generate_password(user_choice, number_of_characters)
-    print(f"Password: {password}")
+    while True:
+        print("Welcome to Password Generator!\n")
+        user_choice, number_of_characters = take_user_choice()
+        password = generate_password(user_choice, number_of_characters)
+        print(f"Password: {password}")
+        return password
 
-run()
+if __name__ == "__main__":
+    run()
+
 
 
     
---
+++ password_generator/validate_user_input.py
@@ -0,0 +1,122 @@
+import string
+
+number_of_characters = 8
+include_numbers = -1
+include_lower_case = -1
+include_upper_case = -1
+include_symbols = -1
+validation_list_of_1_or_0 = [1,0]
+
+def take_and_validate_input_password():
+    """Takes in and validates the user's password on set of conditions"""
+    lowercase_alphabets = string.ascii_lowercase
+    uppercase_alphabets = string.ascii_uppercase
+    numbers = string.digits
+    symbols = string.punctuation
+    track_passwd_parameters = set()
+    
+    while True:
+
+        input_password = input("Enter password: ")
+
+        for character in input_password:
+            if character in numbers:
+                track_passwd_parameters.add('a')
+            if character in lowercase_alphabets:
+                track_passwd_parameters.add('b')
+            if character in uppercase_alphabets:
+                track_passwd_parameters.add('c')
+            if character in symbols:
+                track_passwd_parameters.add('d')
+
+        if len(track_passwd_parameters) < 4:
+            track_passwd_parameters = set()
+            print("The password should contain at least one symbol, uppercase, lowercase, and number.")
+        elif len(input_password) < 8:
+            print("The password should contain at least 8 characters.")
+        elif (len(track_passwd_parameters) == 4 and len(input_password) > 8):
+            break
+    return input_password
+
+def take_and_validate_length_of_characters():
+    """Takes in length_of_character and validates it, loops until the user provides the accepted input"""
+    
+    while True:
+        try:
+            number_of_characters = int(input("Length of Password: "))            
+            if 8 < number_of_characters < 32:
+                break
+            if number_of_characters < 8 or number_of_characters > 32:
+                print("Please input password length atleast 8 characters and atmost 32 characters.")
+
+        except ValueError:
+            print("Invalid Input! Please provide an integer.")
+
+    return number_of_characters
+
+def take_and_validate_input_numbers():
+    """Takes in length_of_character and validates it, loops until the user provides the accepted input"""
+    
+    while True:
+        try:
+            include_numbers = int(input("Do you want to include numbers: "))
+            if include_numbers in validation_list_of_1_or_0:
+                break
+            if include_numbers not in validation_list_of_1_or_0:
+                print("Please input either 1 or 0.")
+
+        except ValueError:
+            print("Invalid Input! Please provide an integer.")
+
+    return include_numbers
+
+def take_and_validate_input_lower_case():
+    """Takes in length_of_character and validates it, loops until the user provides the accepted input"""
+    
+    while True:
+        try:
+            include_lower_case = int(input("Do you want to include lower case characters: "))
+            if include_lower_case in validation_list_of_1_or_0:
+                break
+            if include_lower_case not in validation_list_of_1_or_0:
+                print("Please input either 1 or 0.")
+
+        except ValueError:
+            print("Invalid Input! Please provide an integer.")
+
+    return include_lower_case
+
+def take_and_validate_input_upper_case():
+    """Takes in length_of_character and validates it, loops until the user provides the accepted input"""
+    
+    while True:
+        try:
+            include_upper_case = int(input("Do you want to include upper case characters: "))
+            if include_upper_case in validation_list_of_1_or_0:
+                break
+            if include_upper_case not in validation_list_of_1_or_0:
+                print("Please input either 1 or 0.")
+
+        except ValueError:
+            print("Invalid Input! Please provide an integer.")
+
+    return include_upper_case
+
+def take_and_validate_input_symbols():
+    """Takes in length_of_character and validates it, loops until the user provides the accepted input"""
+    
+    while True:
+        try:
+            include_symbols = int(input("Do you want to include symbols: "))
+            if include_symbols in validation_list_of_1_or_0:
+                break
+            if include_symbols not in validation_list_of_1_or_0:
+                print("Please input either 1 or 0.")
+
+        except ValueError:
+            print("Invalid Input! Please provide an integer.")
+
+    return include_symbols
+
+if __name__ == '__main__':
+    take_and_validate_input_password()
